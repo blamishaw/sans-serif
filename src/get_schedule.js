e4075f8c1bd0ca1c4058481b3d5410e67e61a3dd
Brendan Lamishaw <brendanlamishaw@Brendans-MacBook-Pro.local>
2020-04-03 17:37:09 -0700

    scheduling algorithm

--- src/get_schedule.js
+++ src/get_schedule.js
@@ -1,9 +1,11 @@
 // Day constants for matrix accessing
-const MONDAY = 0;
-const TUESDAY = 1;
-const WEDNESDAY = 2;
-const THURSDAY = 3;
-const FRIDAY = 4;
+const class_days = {
+  "Mo": 0,
+  "Tu": 1,
+  "We": 2,
+  "Th": 3,
+  "Fr": 4,
+};
 
 // Time constants for matrix using 24hr time -- 8AM to 10PM in half-hour intervals
 const class_times = {
@@ -50,16 +52,70 @@ const proxy_url = 'https://cors-anywhere.herokuapp.com/';
 // Base API url
 const base_url = "http://api.asg.northwestern.edu/courses/details/?key=" + config.API_key;
 
-const roundTime = (time) => {
-  let [hours, minutes] = time.split(':');
+// To check if a time has already been allocated, we organize the week into a 28x5 matrix
+// Length: 28 half-hour intervals between 8AM and 10PM
+// Width: 5 days of the school week
+let calendar = init_matrix();
+
+function init_matrix() {
+  let time_matrix = [];
+  for (let i = 0; i < 29; i++){
+    time_matrix.push([0, 0, 0, 0, 0]);
+  }
+  return time_matrix;
+}
+
+const alloc_time = (start_time, end_time, meeting_days) => {
+  for (let day of meeting_days) {
+    for (let i = start_time; i < end_time; i++){
+      calendar[class_times[i]][class_days[day]] = 1;
+      console.log(calendar);
+    }
+  }
+}
+
+const is_alloc = (start_time, end_time, meeting_days) => {
+
+  for (let day of meeting_days){
+    let day_col = calendar.map(col => col[class_days[day]]);
+    let time_of_class = day_col.slice(start_time, end_time);
+
+    if (time_of_class.includes(1)) return false;
+  }
+  return true;
+
+}
+
+// Rounds a class start/end time to the nearest half hour
+const round_time = (time) => {
+  let [hour, minutes] = time.split(':');
   if (minutes == "20") {
     minutes = "30";
   }
-  console.log(hours + ":" + minutes);
+  if (minutes == "50"){
+    minutes = "00";
+    if (hour == "08"){
+      hour = "09";
+    }
+    else hour = (parseInt(hour) + 1).toString();
+  }
 }
 
+// Determines course meeting days and start/end times. Calls is_alloc to determine
+// if the meeting times of the class are already allocated
 const check_availaible = (course) => {
-  console.log(roundTime(course.start_time), roundTime(course.end_time));
+  const meeting_days = course.meeting_days.match(/[A-Z][a-z]+/g);
+  const start_time = round_time(course.start_time);
+  const end_time = round_time(course.end_time);
+
+  const available = is_alloc(start_time, end_time, meeting_days);
+  if (available) {
+    alloc_time(start_time, end_time, meeting_days);
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Removes an item from the user-input course list so it isn't reused
@@ -74,9 +130,32 @@ const remove_item_from_courses = (courses, item) => {
 // Selects a random course from the course JSON
 // This will need to be changed to accomodate course prerequisites
 const select_random_course = (json) => {
-  const course = json[Math.floor(Math.random() * json.length)];
-  check_availaible(course);
-  return;
+  if (json.length == 0){
+    console.log(`ERROR: No courses found`);
+    return null;
+  }
+
+  let course = json[Math.floor(Math.random() * json.length)];
+
+  // Some courses have incomplete data -- no start/end times -- must search the whole list to find one
+  let i;
+  for (i = 0; (!course.start_time && i < json.length); i++) {
+    course = json[Math.floor(Math.random() * json.length)];
+    try {
+      let allocated = check_availaible(course);
+      if (allocated) break;
+    }
+    catch(err) {
+      continue;
+    }
+  }
+
+  if (i == json.length) {
+    console.log(`No courses found in subject ${course.subject}.`);
+    return null;
+  }
+
+  return course;
 }
 
 // Fetches Northwestern course JSON data given a subject and term – then selects
@@ -88,6 +167,8 @@ const fetch_courses = async (term, subject) => {
   if (response.ok) {
     let json = await response.json();
     let random_course = select_random_course(json);
+    console.log(random_course);
+    console.log(calendar);
     return random_course;
   }
   return null;
@@ -111,29 +192,4 @@ const process_courses = async (courses) => {
     }
 
   }
-}
-
-
-let time_matrix = []
-
-const init_matrix = () => {
-  for (let i = 0; i < 27; i++){
-    time_matrix.push([0, 0, 0, 0, 0]);
-  }
-}
-
-const alloc_time = (time, days) => {
-  time_matrix[class_times[time]][day] = 1;
-}
-
-const free_time = (time, day) => {
-  time_matrix[class_times[time]][day] = 0;
-}
-
-const is_alloc = (start_time, end_time, days) => {
-  for (let day of days){
-    let day_col = time_matrix.map(col => col[day]);
-    console.log(day_col);
-  }
-  // console.log(time_matrix[class_times[time]][days[0]];
 }
